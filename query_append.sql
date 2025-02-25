32c0eee66119b54dcf8d12f71a33860214ddcc30
xtern0o <dos30028@gmail.com>
2025-02-25 14:45:03 +0300

    [fix]

--- query_append.sql
+++ query_append.sql
@@ -39,7 +39,7 @@ INSERT INTO dinosaur (name, gender, age, dino_type_id, current_swarm_id, parent_
     (DEFAULT, 'f', 30, 2, 1, NULL, 1),
     ('максим', 'm', 19, 1, 1, 1, 3),
     ('еще утконос', 'm', 4430, 2, 1, 2, 2),
-    ('Грант', 'm', 20, 1, 2, 3, 2, 3);
+    ('Грант', 'm', 20, 1, 2, 3, 2);
 
 INSERT INTO dino_to_action (dino_id, action_id) VALUES
     (1, 1),
@@ -56,4 +56,4 @@ INSERT INTO dinotype_to_diet (dinotype_id, diet_id) VALUES
     (1, 1),
     (1, 4),
     (2, 2),
-    (2, 3),
+    (2, 3);
